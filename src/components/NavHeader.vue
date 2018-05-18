7c1518484c5e32733f45fe97eab4c164bc2450bc
rocklu <624826870@qq.com>
2018-05-19 01:03:29 +0800

    登录框以及logo更改

--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -10,7 +10,7 @@
         <div class="navbar">
           <div class="navbar-left-container">
             <a href="/">
-              <img class="navbar-brand-logo" src="static/logo.png"></a>
+              <img class="navbar-brand-logo" src="static/logo.jpg" width="105"></a>
           </div>
           <div class="navbar-right-container" style="display: flex;">
             <div class="navbar-menu-container">
